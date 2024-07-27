4987259d3c89de7384f36be6c466a065ec654015
muhziddan <ziddan.bsi@gmail.com>
2024-07-27 13:27:18 +0700

    initial project

---
+++ GrabSixthSense/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrabSixthSense
+//
+//  Created by Muhammad Ziddan on 27/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
