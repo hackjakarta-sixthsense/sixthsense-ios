88c305a6cd47eecce3e8738106315e57311c718e
muhziddan <ziddan.bsi@gmail.com>
2024-07-27 16:03:44 +0700

    add method subview for uiview, add maps and backbutton for maps view

---
+++ GrabSixthSense/Modules/Transport/Component/CircleFloatingButton.swift
@@ -0,0 +1,12 @@
+//
+//  CircleFloatingButton.swift
+//  GrabSixthSense
+//
+//  Created by Muhammad Ziddan on 27/07/24.
+//
+
+import UIKit
+
+class CircleFloatingButton: UIButton {
+    
+}
--- GrabSixthSense/Modules/Transport/TransportViewController.swift
+++ GrabSixthSense/Modules/Transport/TransportViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class TransportViewController: ViewController {
+    
     private var viewModel: TransportViewModel
+    
+    private let backgroundView = UIView()
+    private let mapView = GMSMapView()
+    private let bottomView = UIView()
+    private let backButton = CircleFloatingButton()
     
     init(viewModel: TransportViewModel) {
         self.viewModel = viewModel
@@ -21,7 +28,19 @@ class TransportViewController: ViewController {
     
     internal override func viewDidLoad() {
         super.viewDidLoad()
+        setupBackgroundView()
     }
+    
+    private func setupBackgroundView() {
+        view.addSubview(backgroundView)
+        backgroundView.backgroundColor = .white
+        backgroundView.constraints(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        backgroundView.addSubviews([mapView, backButton])
+    }
+    
+}
+
+extension TransportViewController {
     
     func assignState(state: ApiState) {
         
