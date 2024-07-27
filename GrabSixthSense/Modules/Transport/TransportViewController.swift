cbd599c9d674bee816d3514fc60f90cf4a171cd2
muhziddan <ziddan.bsi@gmail.com>
2024-07-27 15:30:44 +0700

    create base for maps view

--- GrabSixthSense/Modules/Transport/TransportViewController.swift
+++ GrabSixthSense/Modules/Transport/TransportViewController.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class TransportViewController: ViewController {
+    private var viewModel: TransportViewModel
+    
+    init(viewModel: TransportViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func assignState(state: ApiState) {
+        
+    }
+    
+}
